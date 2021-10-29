7240addf6cdb2c16020344dbda6fa0dad37e6625
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-10-29 10:02:48 -0400

    Begin HW 5-2

---
+++ hw5-2.py
@@ -0,0 +1,22 @@
+# Author: CRS 10/29/21
+import math
+# Question 1
+math.sqrt(529)
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
+
+# Question 2
